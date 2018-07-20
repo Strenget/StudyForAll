230e2983842d9658679a31280100d27210cf6eb8
Kseniia Bashkova <nikitaqwert5@gmail.com>
2018-07-20 14:24:54 +0200

    no message

--- StudyTests/HelpForStart/Properties/StaticMassive.cs
+++ StudyTests/HelpForStart/Properties/StaticMassive.cs
@@ -4,31 +4,12 @@
 	{
 		public int[] AddEvenNumbers(int[] numbers) //Дан массив чисел, нужно создать из данного массива массив в котором хранятся только четные цифры
 		{
-            int lenNum = numbers.Length;
-            int[] numCet = new int[lenNum];
-            foreach (int i in numbers)
-            {
-                if (numbers[i] % 2 == 0)
-                {
-                    numbers[i] = numCet[i];
-                }
-            }
-
-            return new[] {1}; //Небходимо вернуть массив типа int[]
+			return new[] {1}; //Небходимо вернуть массив типа int[]
 		}
 
 		public int[] AddOddNumbers(int[] numbers) //Дан массив чисел, нужно создать из данного массивa массив в котором хранятся только нечетные цифры
 		{
-            int lenNum = numbers.Length;
-            int[] numCet = new int[lenNum];
-            foreach (int i in numbers)
-            {
-                if (numbers[i] % 2 != 0)
-                {
-                    numbers[i] = numCet[i];
-                }
-            }
-            return new[] {1}; //Небходимо вернуть массив типа int[]
+			return new[] {1}; //Небходимо вернуть массив типа int[]
 		}
 
 		public int[] BreakingPassword(int number) //Задача взломать пароль методом перебора (Brute Force), на входе дано число,
@@ -41,18 +22,6 @@
 																															//будет означать, что на данном индексе знаки одинаковы, а знак '^'
 																															//там где знаки отличаются => Например first = "Nikita" a second = "Nakata" ответ здесь будет "-^----"
 		{
-            int len = first.Length;
-
-            string[] compare = new string[len];
-
-            for (int i = 0; i < len; i++)
-            {
-                if (first[i] == second[i])
-                {
-                    compare[i] = "-";
-                }
-                else compare[i] = "^";
-            }
 			return string.Empty;
 		}
 	}
